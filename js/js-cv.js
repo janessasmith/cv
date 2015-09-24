d11a6f689383a71b1ba42ebfce98b8ea30ce5543
janessasmith <jupiter_zheng@sina.com>
2015-09-24 15:54:00 +0800

    update code

--- js/js-cv.js
+++ js/js-cv.js
@@ -21,11 +21,6 @@ var isTouchDevice = Modernizr.touch ||
     deviceAgent.match(/ipod/i) ||
     deviceAgent.match(/blackberry/i) ||
     deviceAgent.match(/bada/i));
-//if (isTouchDevice) {
-//    //Do something touchy
-//} else {
-//    //Can't touch this
-//}
 
 function loadOut() {
     // initialization
